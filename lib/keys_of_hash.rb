b2b99d425f5ba400e8ad84332e5caadcc8a39bea
Daniel Ran <danielr1735@gmail.com>
2018-06-23 05:54:45 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,17 @@
+# class Hash
+#   def keys_of(*arguments)
+#     args = [*arguments].flatten
+#     array = []
+#       args.each do |arg|
+#         array.push(self.select { |key, value| value == arg }.keys)
+#       end
+#     array.flatten
+#   end
+# end
+
+
 class Hash
   def keys_of(*arguments)
-    args = [*arguments].flatten
-    array = []
-      args.each do |arg|
-        array.push(self.select { |key, value| value == arg }.keys)
-      end
-    array.flatten
+    map {|key, value| args.include?(value) ? key : nil }.compace
   end
 end
